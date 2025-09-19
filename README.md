7a01403571c9b56c1be4f7d0c11ecb9db19a0d09
Danush Ramanan <dan.ramanan@gmail.com>
2025-09-18 21:00:00 -0600

    yessir

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # TODO
-- save image of barbell with weight on top
 - kg mode
 - footer (like my github and stuff)
--- script.js
+++ script.js
@@ -2,20 +2,53 @@ const PLATE_GAP_MAX = 100;
 const PLATE_GAP_MIN = 20;
 const PLATE_GAP_DECAY_FACTOR = 0.04;
 
+const barbellOptions = [
+    { lbs: 45, kg: 20 },
+    { lbs: 35, kg: 15 },
+    { lbs: 15, kg: 7 }
+]
+const plateOptions = [
+    { lbs: 55, kg: 25 },
+    { lbs: 45, kg: 20},
+    { lbs: 35, kg: 15 },
+    { lbs: 25, kg: 10 },
+    { lbs: 10, kg: 5 },
+    { lbs: 5, kg: 2.5 },
+    { lbs: 2.5, kg: 1.25 },
+    { lbs: 1, kg: 0.5 },
+    { lbs: 0.5, kg: 0.25 }
+]
+
 let barbell;
 let plates = [];
+let units = "lbs";
 
-let weightDisplay = document.querySelector("#weight-display");
-let weightInput = document.querySelector("#weight-input")
-let barbellWeightDropdown = document.querySelector("#barbell-weight");
-let barbellSides = document.querySelectorAll(".barbell-side");
-let barbellDisplay = document.querySelector("#barbell");
+const weightDisplay = document.querySelector("#weight-display");
+const weightInput = document.querySelector("#weight-input")
+const barbellWeightDropdown = document.querySelector("#barbell-weight");
+const barbellSides = document.querySelectorAll(".barbell-side");
+const barbellDisplay = document.querySelector("#barbell");
+const kgToggle = document.querySelector("#kg-toggle");
+const plateLabels = document.querySelectorAll(".plate-label");
 
 class PlateStack {
     constructor(weight, number) {
         this.weight = weight;
         this.number = number;
     }
+}
+
+function setUnits() {
+    // weight dropdown
+    let options = barbellWeightDropdown.children;
+    for (let i = 0; i < options.length; i++) {
+        const weight = barbellOptions[i][units]; 
+        options[i].value = weight;
+        options[i].textContent = weight + " " + units;
+    }
+
+    // plates
+    // TODO: implement above for each child of the plateLabels
 }
 
 function calculateWeights(weight) {
@@ -39,7 +72,7 @@ function calculateWeights(weight) {
 
     if (perSide !== 0) {
         return {
-            error: `Could not match exact target with given plates. ${perSide} lbs leftover per side.`,
+            error: `Could not match exact target with given plates. ${perSide} ${units} leftover per side.`,
             plates: result
         }
     }
@@ -60,7 +93,7 @@ function updatePlateList(plateWeight, isChecked) {
 
 function updateBarbellWeight(weight) {
     barbell = weight;
-    let message = barbell + " lbs";
+    let message = barbell + " " + units;
     weightDisplay.textContent = message;
     barbellDisplay.title = message;
 }
@@ -70,7 +103,7 @@ function updateUI() {
     let result = calculateWeights(weight);
 
     if (result.plates) {
-        weightDisplay.textContent = weight + " lbs";
+        weightDisplay.textContent = weight  + " " + units;
         
         barbellSides.forEach(side => {
             side.innerHTML = "";
@@ -80,8 +113,8 @@ function updateUI() {
                     const plate = document.createElement("div");
                     plate.classList.add("plate-shape", `plate-${plateStack.weight}`);
                     
-                    plate.title = `${plateStack.weight} lbs`;
-                    if (plateStack.weight === 1) // removing the 's' from lbs if its only 1 lb
+                    plate.title = plateStack.weight + " " + units;
+                    if (plateStack.weight === 1 && units === "lbs") // removing the 's' from lbs if its only 1 lb
                         plate.title = plate.title.slice(0, -1);
 
                     side.appendChild(plate);
@@ -95,16 +128,25 @@ function updateUI() {
         const stackPercent = (sideWidth / barWidth) * 100;
 
         let gapValue = PLATE_GAP_MAX * Math.exp(-PLATE_GAP_DECAY_FACTOR * stackPercent); // exponential decay with stackPercent
-        console.log(gapValue);
         gapValue = Math.max(PLATE_GAP_MIN, Math.min(PLATE_GAP_MAX, gapValue)); // clamp value
-        console.log(gapValue);
         barbellDisplay.style.gap = `${gapValue}%`;
     }
     if (result.error)
         weightDisplay.textContent = result.error;
 }
 
+kgToggle.addEventListener("click", () => {
+    if (units === "lbs") {
+        units = "kg";
+        kgToggle.textContent = "Switch to lbs";
+    }
+    else {
+        units = "lbs";
+        kgToggle.textContent = "Switch to kg"
+    }
 
+    updateBarbellWeight(barbell);
+});
 document.querySelector("#calculate-btn").addEventListener("click", updateUI);
 weightInput.addEventListener("keydown", (event) => {
     if (event.key !== "Enter")
@@ -115,7 +157,8 @@ weightInput.addEventListener("keydown", (event) => {
 barbellWeightDropdown.addEventListener("change", (event) => {
     updateBarbellWeight(parseFloat(event.target.value));
 });
-document.querySelectorAll(".plate-label input").forEach(input => {
+plateLabels.forEach(plateLabel => {
+    let input = plateLabel.querySelector("label"); //TODO: check if this works
 	input.addEventListener("change", (event) => {
 		updatePlateList(parseFloat(event.target.value), event.target.checked);
 	});
